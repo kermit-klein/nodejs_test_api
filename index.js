0c2ba9866f59d9100daf1b234235dbc7682cdb7a
allezz <aerbay@gmail.com>
2020-10-19 22:51:50 +0200

    Add get request and listen to port

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const port = 3000
 app.use(bodyParser.json())
 
 app.use(bodyParser.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+    res.json({info:"Hello from Express"})
+})
+
+app.listen(port,()=>{
+    console.log(`App is running on ${port}`)
+})
