12b8f8deabf885911cf00a63a41553fa28f7f23b
allezz <aerbay@gmail.com>
2020-10-20 01:49:18 +0200

    Add delete user query

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import {getUsers,createUser,deleteUser,updateUser} from "./queries"
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
@@ -10,6 +12,13 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.get("/",(req,res)=>{
     res.json({info:"Hello from Express"})
 })
+
+app.get('/users', getUsers)
+app.get('/users/:id', getUserById)
+app.post('/users', createUser)
+app.put('/users/:id', updateUser)
+app.delete('/users/:id', deleteUser)
+
 
 app.listen(port,()=>{
     console.log(`App is running on ${port}`)
--- queries.js
+++ queries.js
@@ -33,7 +33,6 @@ const createUser = (req,res) => {
 const updateUser = (req,res) => {
     const id = parseInt(req.params.id)
     const {name,email} = req.body
-
     pool.query("UPDATE users SET name=$1,email=$2 WHERE id = $3",[name,email,id],(error,results)=>{
         if(error) {
             throw error
@@ -41,3 +40,15 @@ const updateUser = (req,res) => {
         res.status(200).send(`User modified with ID:${id}`)
     })
 }
+
+const deleteUser = (req,res) => {
+    const id = parseInt(req.params.id)
+    pool.query("DELETE FROM users WHERE id=$1",[id],(error,results)=>{
+        if(error){
+            throw error
+        }
+        res.status(200).send(`User deleted with ID:${id}`)
+    })
+}
+
+export {getUsers,createUser,deleteUser,updateUser}
