9b1f7252e31fd21a6fc7e6fb3e6498ce88b97890
allezz <aerbay@gmail.com>
2020-10-20 00:26:02 +0200

    Add createUser query

--- queries.js
+++ queries.js
@@ -19,3 +19,13 @@ const getUsers = (req,res) => {
         res.status(200).json(results.rows)
     })
 }
+
+const createUser = (req,res) => {
+    const {name,email} = req.body
+    pool.query('INSERT INTO user (name,email) VALUES ($1,$2)',[name,email], (error,results)=>{
+        if(error) {
+            throw error
+        }
+        res.status(201).send(`User added with ID: ${results.insertId}`)
+    })
+}
